502b08116b16ae82c6df3fc2ef804a4d142967df
Muneeb Ali <muneeb@ali.vc>
2015-10-16 13:55:10 -0400

    added basic unit tests

---
+++ tests/unit_tests.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Proofchecker
+    ~~~~~
+
+    copyright: (c) 2014 by Halfmoon Labs, Inc.
+    copyright: (c) 2015 by Blockstack.org
+
+This file is part of Proofchecker.
+
+    Registrar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Registrar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Proofchecker. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os
+import sys
+import unittest
+import requests
+
+# Hack around absolute paths
+current_dir = os.path.abspath(os.path.dirname(__file__))
+parent_dir = os.path.abspath(current_dir + "/../")
+sys.path.insert(0, parent_dir)
+
+from proofchecker import profile_to_proofs
+
+test_users = ['muneeb', 'fredwilson']
+
+BASE_URL = 'http://resolver-btc.onename.com/v2/users/'
+
+
+def get_profile(username):
+
+    resp = requests.get(BASE_URL + username, timeout=10, verify=False)
+
+    data = resp.json()
+
+    return data[username]['profile']
+
+
+class ProofcheckerTestCase(unittest.TestCase):
+
+    def tearDown(self):
+        pass
+
+    def test_twitter_proof(self):
+        """ Check twitter proof
+        """
+
+        for username in test_users:
+            profile = get_profile(username)
+            proofs = profile_to_proofs(profile, username)
+
+            for proof in proofs:
+
+                if proof['service'] == 'twitter':
+                    self.assertTrue(proof['valid'])
+
+if __name__ == '__main__':
+
+    unittest.main()
