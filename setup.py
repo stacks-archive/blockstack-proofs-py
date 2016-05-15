6487c554d7a95df728daa87da73031bfbbe4c7be
Muneeb Ali <muneeb@ali.vc>
2016-05-14 20:00:55 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blockstack-proofs',
-    version='0.0.4',
+    version='0.0.5',
     url='https://github.com/blockstack/blockstack-proofs',
     license='MIT',
     author='Blockstack.org',
